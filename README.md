c4ac40cd73de3c0ae8604b83cf82c02abb08bb88
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2018-02-06 18:27:54 +0100

    always return plain text. double-validate filename

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ A simple HTTP endpoint to upload distfiles from Travis CI.
     $ curl ${TOKEN}@localhost/ \
           -F 'file=@blogc-0.13.0.tar.gz' \
           -F 'project=blogc' \
+          -F 'version=0.13.0' \
           -F "sha512=$(sha512sum blogc-0.13.0.tar.gz)"
--- distfiles_api.py
+++ distfiles_api.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from flask import Flask, abort, request
+from flask import Flask, request
 import hashlib
 import os
 import re
@@ -8,7 +8,7 @@ import re
 app = Flask(__name__)
 
 app.config.update(
-    AUTH_TOKEN='bola',
+    AUTH_TOKEN='token',
     DISTFILES_BASEDIR=os.getcwd(),
 )
 app.config.from_envvar('DISTFILES_CONFIG', True)
@@ -16,35 +16,48 @@ app.config.from_envvar('DISTFILES_CONFIG', True)
 re_sha512 = re.compile(r'([0-9a-f]{128}) (\*| )(.+)')
 
 
+def abort400():
+    return 'BAD_REQUEST\n', 400, {'Content-Type': 'text/plain'}
+
+
+def abort401():
+    return 'UNAUTHORIZED\n', 401, {'Content-Type': 'text/plain'}
+
+
 @app.route('/', methods=['POST'])
 def upload():
     if request.authorization is None:
-        abort(401)
+        return abort401()
     if request.authorization.username != app.config['AUTH_TOKEN']:
-        abort(401)
+        return abort401()
 
     if 'file' not in request.files:
-        abort(400)
+        return abort400()
     if any(i not in request.form for i in ('project', 'version', 'sha512')):
-        abort(400)
+        return abort400()
 
     match_sha512 = re_sha512.match(request.form['sha512'])
     if match_sha512 is None:
-        abort(400)
+        return abort400()
 
     hash_sha512, binary_flag, filename = match_sha512.groups()
 
     if len(filename) < 4:
-        abort(400)
+        return abort400()
     if any(i in filename for i in '/\\'):
-        abort(400)
+        return abort400()
+
+    fileobj = request.files['file']
+
+    if filename != fileobj.filename:
+        return abort400()
 
     # this should waste some memory, but it is ok. also, upload file size
-    # should be restricted in nginx.
-    data = request.files['file'].read()
+    # should be already restricted by nginx.
+    data = fileobj.read()
 
     if hash_sha512 != hashlib.sha512(data).hexdigest():
-        abort(400)
+        return abort400()
 
     p = '%s-%s' % (request.form['project'], request.form['version'])
     destdir = os.path.join(app.config['DISTFILES_BASEDIR'],
@@ -68,4 +81,4 @@ def upload():
 
     os.symlink(p, latest)
 
-    return 'OK\n'
+    return 'OK\n', 200, {'Content-Type': 'text/plain'}
